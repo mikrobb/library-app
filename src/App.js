b87040cfe1e4c2d70709d2680e0bbc879ca656e1
mikrob <promikrob@gmail.com>
2023-01-13 18:00:04 +0200

    start homepage html

--- src/App.js
+++ src/App.js
@@ -1,40 +1,16 @@
 import "./App.css";
-import { useState, useRef, useEffect } from "react";
 
-// const url = "https://www.googleapis.com/books/v1/volumes?q=php";
+import { Routes, Route } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
+import Homepage from "./componentsJs/Homepage";
 
 function App() {
-  const [search, setSearch] = useState("php");
-  const [books, setBooks] = useState([]);
-  const [newSeacrh, setNewSearch] = useState("");
-
-  useEffect(() => {
-    fetch(`https://www.googleapis.com/books/v1/volumes?q=${search}`)
-      .then((data) => data.json())
-      .then((json) => setBooks(json.items));
-  }, [setBooks, search]);
-
-  console.log(search);
-  console.log(books);
-
-  function GetBooks() {
-    // setSearch(newSeacrh);
-  }
-
-  if (!books) return <div>Loading...</div>;
   return (
-    <>
-      <div>
-        <input type="text" onChange={(e) => setSearch(e.target.value)} />
-        <button onClick={() => GetBooks}>Submit</button>
-      </div>
-
-      <div>
-        {books.map((info) => (
-          <div>{info.volumeInfo.title}</div>
-        ))}
-      </div>
-    </>
+    <HashRouter>
+      <Routes>
+        <Route exact path="/" element={<Homepage />} />
+      </Routes>
+    </HashRouter>
   );
 }
 
