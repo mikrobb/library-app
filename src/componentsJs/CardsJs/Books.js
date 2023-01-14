1bcf4f4137a90eb960d8db41c17a4cf6b1fecf17
mikrob <promikrob@gmail.com>
2023-01-14 17:16:20 +0200

    Html and css

--- src/componentsJs/CardsJs/Books.js
+++ src/componentsJs/CardsJs/Books.js
@@ -1,10 +1,29 @@
 import React from "react";
+import "../../componentsCss/CardsCss/Books.css";
 
 export default function Books({ info }) {
+  console.log(info.saleInfo.listPrice);
   return (
-    <>
-      <div>{info.volumeInfo.title}</div>
-      <div></div>
-    </>
+    <div className="containerBook">
+      <div>
+        <img
+          className="imgBook"
+          src={info.volumeInfo.imageLinks.smallThumbnail}
+          alt=""
+        />
+        <div className="titleBook">{info.volumeInfo.title}</div>
+        <div className="authorBook">{info.volumeInfo.publisher}</div>
+        <div className="priceBook">
+          {!info.saleInfo.listPrice
+            ? info.saleInfo.saleability
+            : info.saleInfo.listPrice.amount +
+              " " +
+              info.saleInfo.listPrice.currencyCode}
+        </div>
+      </div>
+      <div>
+        <div className="btnView">View more</div>
+      </div>
+    </div>
   );
 }
